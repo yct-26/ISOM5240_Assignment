7cec06d87a42587d2da64a573de9295096ae4e5e
yct-26 <issactsang2686@gmail.com>
2026-05-08 12:41:31 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+# Main part
+st.set_page_config(page_title="Your Image to Audio Story", page_icon="🧠")
+st.header("Turn Your Image to Audio Story")
+uploaded_file = st.file_uploader("Select an Image...")
