b215b77700397c4db5a4b5c1b206e6e6ad6426a5
yct-26 <issactsang2686@gmail.com>
2026-05-08 12:50:59 +0800

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,58 @@
-# Main part
+# Setting Headers and Titles for the Application
 st.set_page_config(page_title="Your Image to Audio Story", page_icon="🧠")
 st.header("Turn Your Image to Audio Story")
+
+# Adding a file uploader to the application
 uploaded_file = st.file_uploader("Select an Image...")
+
+
+# Defining a function to transform image to text
+def img2text(url):
+    image_to_text_model = pipeline("image-to-text", 
+                                   model="Salesforce/blip-image-captioning-base")
+    text = image_to_text_model(url)[0]["generated_text"]
+    return text
+
+# Defining a function to generate a story from text
+def text2story(text):
+    story_pipe = pipeline("text-generation", 
+                            model="pranavpsv/genre-story-generator-v2")
+    story_results = story_pipe(text)
+    story = story_results[0]['generated_text']
+    return story_text
+
+# Defining a function to transform story to speech/audio
+def text2audio(story_text):
+    audio_pipe = pipeline("text-to-audio", 
+                          model="Matthijs/mms-tts-eng")
+    audio_data = audio_pipe(story)
+    return audio_data
+
+
+# Main part to run the code
+if uploaded_file is not None:
+    scenario = img2text(uploaded_file.name)
+    st.write(f"**Scenario:** {scenario}")
+    story_text = text2story(scenario)
+    st.write(f"**Story:** {story}")
+    audio_data = text2audio(story_text)
+
+
+    # Play button
+if st.button("Play Audio"):
+    audio_array = audio_data["audio"]
+    sample_rate = audio_data["sampling_rate"]
+    st.audio(audio_array, sample_rate=sample_rate)
+   
+
+
+
+
+
+
+
+
+
+
+
+
